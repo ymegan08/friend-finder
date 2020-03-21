bc637f8ac8739b50af252f5fe36cf9dd4d7335b8
Megan Cho <ymegan08@gmail.com>
2020-03-20 19:35:57 -0700

    resize images

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,20 +10,18 @@ module.exports = function(app){
 	app.post("/api/friends", function(req, res) {
 		console.log(req.body.scores);
 	
-		// Receive user details (name, photo, scores)
-		var user = req.body;
+		// Receive user details
+		var userInput = req.body;
 	
 		// parseInt for scores
 		for(var i = 0; i < user.scores.length; i++) {
 		  user.scores[i] = parseInt(user.scores[i]);
 		}
 	
-		// default friend match is the first friend but result will be whoever has the minimum difference in scores
 		var bestFriendIndex = 0;
 		var minimumDifference = 40;
 	
-		// in this for-loop, start off with a zero difference and compare the user and the ith friend scores, one set at a time
-		//  whatever the difference is, add to the total difference
+		// Start with zero difference and compare the user and the ith friend scores
 		for(var i = 0; i < friends.length; i++) {
 		  var totalDifference = 0;
 		  for(var j = 0; j < friends[i].scores.length; j++) {
@@ -31,15 +29,15 @@ module.exports = function(app){
 			totalDifference += difference;
 		  }
 	
-		  // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
+		  // If there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
 		  if(totalDifference < minimumDifference) {
 			bestFriendIndex = i;
 			minimumDifference = totalDifference;
 		  }
 		}
 	
-		// after finding match, add user to friend array
-		friends.push(user);
+		// Add user to friend array
+		friends.push(userInput);
 	
 		// send back to browser the best friend match
 		res.json(friends[bestFriendIndex]);
