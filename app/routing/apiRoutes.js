a762b9667d0752e6eba5c79f4d733eea196ddbe8
Megan Cho <ymegan08@gmail.com>
2020-03-20 19:25:58 -0700

    changes to api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,3 @@
-// Dependencies
-var path = require("path");
-
 // Import friend entires
 var friends = require('../data/friends.js');
 
@@ -10,49 +7,41 @@ module.exports = function(app){
 		res.json(friends);
     });
     // Post to handle incoming survey results and compatibility logic
-    app.post('/api/friends', function(req, res){
-        // Store user input
-        var userInput = req.body;
-        console.log('userInput = ' + JSON.stringify(userInput));
-        var userResponses = userInput.scores;
-        console.log('userResponses = ' + userResponses);
-
-        // Convert scores to integers
-        for (var i = 0; i < userResponses.length; i++){
-            userResponses[i] = parseInt(userResponses[i]);
-        }
-
-        // Compute best friend match
-		var matchName = '';
-		var matchImage = '';
-		var totalDifference = 100;
-
-		// Examine all existing friends in the list
-		for (var i = 0; i < friends.length; i++) {
-			console.log('friend = ' + JSON.stringify(friends[i]));
-			// Calculate score differences for each question
-			var difference = 0;
-			for (var j = 0; j < userResponses.length; j++) {
-				difference += Math.abs(friends[i].scores[j] - userResponses[j]);
-			}
-			console.log('difference = ' + difference);
-
-			// If lowest difference, record the friend match
-			if (difference < totalDifference) {
-				console.log('Closest match found = ' + difference);
-				console.log('Friend name = ' + friends[i].name);
-				console.log('Friend image = ' + friends[i].photo);
-
-				totalDifference = difference;
-				matchName = friends[i].name;
-				matchImage = friends[i].photo;
-            }
-            
-            // Add new friend
-            friends.push(userInput);
-
-            // Send JSON response
-            res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+	app.post("/api/friends", function(req, res) {
+		console.log(req.body.scores);
+	
+		// Receive user details (name, photo, scores)
+		var user = req.body;
+	
+		// parseInt for scores
+		for(var i = 0; i < user.scores.length; i++) {
+		  user.scores[i] = parseInt(user.scores[i]);
 		}
-    });
+	
+		// default friend match is the first friend but result will be whoever has the minimum difference in scores
+		var bestFriendIndex = 0;
+		var minimumDifference = 40;
+	
+		// in this for-loop, start off with a zero difference and compare the user and the ith friend scores, one set at a time
+		//  whatever the difference is, add to the total difference
+		for(var i = 0; i < friends.length; i++) {
+		  var totalDifference = 0;
+		  for(var j = 0; j < friends[i].scores.length; j++) {
+			var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
+			totalDifference += difference;
+		  }
+	
+		  // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
+		  if(totalDifference < minimumDifference) {
+			bestFriendIndex = i;
+			minimumDifference = totalDifference;
+		  }
+		}
+	
+		// after finding match, add user to friend array
+		friends.push(user);
+	
+		// send back to browser the best friend match
+		res.json(friends[bestFriendIndex]);
+	  });
 }
